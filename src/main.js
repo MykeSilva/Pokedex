78db89cfe5f68f6b6154c4295befe012315c3336
Myke Silva <119154150+Spike2247@users.noreply.github.com>
2024-11-25 21:13:29 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+/* Aula 274 Importando Bibliotecas 04/05/23 01:21 */
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import "../node_modules/bulma/css/bulma.css"
+
+createApp(App).mount('#app')
